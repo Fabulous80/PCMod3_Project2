e9ecdc7fe1c92b65c72e5cd630eaaf3867d5f21c
Marcus Lim <marcz68@gmail.com>
2020-12-08 10:34:14 +0800

    changed name to kueh lapis geenrator

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Colors" component={HomeScreen} />
+      <Stack.Screen name="Kueh Lapis Generator" component={HomeScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 
