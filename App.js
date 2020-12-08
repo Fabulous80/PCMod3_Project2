f9c272f4a9fe7c6c6abea92bd48fa2ea2de2dfb0
Marcus Lim <marcz68@gmail.com>
2020-12-08 10:29:35 +0800

    Added a FlastList and BlockRGB Array

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import BlockRGB from "./components/BlockRGB";
+import { FlatList } from 'react-native-gesture-handler';
 
 function HomeScreen() {
+
+  const [colorArray, setColorArray] = useState([
+    {red: 255, green: 0, blue: 0, id:"0"},
+    {red: 0, green: 255, blue: 0, id:"1"},
+    {red: 0, green: 0, blue: 255, id:"2"},
+
+  ]);
+
+  function renderItem({ item }){
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
   return (
     <View style={styles.container}>
-      <BlockRGB red={255} green={0} blue={0} />
-      <BlockRGB red={0} green={255} blue={0} />
-      <BlockRGB red={0} green={0} blue={255} />
+     <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
     </View>
   );
  }
@@ -34,6 +44,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+
+  list: {
+    width: "100%",
+  },
+ 
 });
