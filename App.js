d3a68ecd221199b40d627cd254052e14688d3890
Marcus Lim <marcz68@gmail.com>
2020-12-08 09:50:20 +0800

    Added homescreen to Stack Navigator

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 
-export default function App() {
+function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  );
+ }
+ 
+ const Stack = createStackNavigator();
+
+ 
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Colors" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+
   );
 }
 
