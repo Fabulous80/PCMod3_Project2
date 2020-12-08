b7d2424bd26dfcc98fe946de379de438d154ce74
Marcus Lim <marcz68@gmail.com>
2020-12-08 11:24:56 +0800

    added the add new color and reset button

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TouchableOpacity} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import BlockRGB from "./components/BlockRGB";
 import { FlatList } from 'react-native-gesture-handler';
+import { set } from 'react-native-reanimated';
 
 function HomeScreen() {
 
@@ -18,8 +19,41 @@ function HomeScreen() {
   function renderItem({ item }){
     return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
   }
+
+  function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: `${colorArray.length}`,
+      },
+    ]);
+  }
+ 
+  function clearColor() {
+    
+      setColorArray([]);
+    
+  }
+
   return (
     <View style={styles.container}>
+       <TouchableOpacity
+       style={{ height: 40, justifyContent: "center" }}
+       onPress={addColor}
+     >
+       <Text style={{ color: "red" }}>Add Colour</Text>
+     </TouchableOpacity>
+
+     <TouchableOpacity
+       style={{ height: 40, justifyContent: "center" }}
+       onPress={clearColor}
+     >
+       <Text style={{ color: "red" }}>Reset Colour</Text>
+     </TouchableOpacity>
+
      <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
     </View>
   );
@@ -27,11 +61,12 @@ function HomeScreen() {
  
  const Stack = createStackNavigator();
 
- 
+
 export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
+    
       <Stack.Screen name="Kueh Lapis Generator" component={HomeScreen} />
     </Stack.Navigator>
   </NavigationContainer>
