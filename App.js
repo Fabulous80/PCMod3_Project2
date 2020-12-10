54eef47bbdeab88714984e16b51a1f8309288541
Marcus Lim <marcz68@gmail.com>
2020-12-10 13:02:00 +0800

    added text style contrast to color codes

--- App.js
+++ App.js
@@ -1,39 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View , TouchableOpacity, Button, useWindowDimensions} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  useWindowDimensions,
+} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
-import { FlatList } from 'react-native-gesture-handler';
-import { set } from 'react-native-reanimated';
-
+import { FlatList } from "react-native-gesture-handler";
+import { set } from "react-native-reanimated";
 
 const NUM_COLUMNS = 5;
 
-
 function HomeScreen({ navigation }) {
-
   const [colorArray, setColorArray] = useState([
-    {red: 255, green: 0, blue: 0, id:"0"},
-    {red: 0, green: 255, blue: 0, id:"1"},
-    {red: 0, green: 0, blue: 255, id:"2"},
-
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 0, green: 255, blue: 0, id: "1" },
+    { red: 0, green: 0, blue: 255, id: "2" },
   ]);
-  const BLOCK_SIZE = useWindowDimensions().width/NUM_COLUMNS;
+  const BLOCK_SIZE = useWindowDimensions().width / NUM_COLUMNS;
 
   useEffect(() => {
     navigation.setOptions({
-      headerRight:() => <Button onPress={addColor} title="Add Color" />,
-      headerLeft:() => <Button onPress={clearColor} title="Reset" />,
+      headerRight: () => <Button onPress={addColor} title="Add Color" />,
+      headerLeft: () => <Button onPress={clearColor} title="Reset" />,
     });
   });
 
-  function renderItem({ item }){
-
+  function renderItem({ item }) {
     return (
-    <TouchableOpacity onPress={() => navigation.navigate("DetailsScreen",{...item})}>
-    <BlockRGB style={{ height: BLOCK_SIZE, width:BLOCK_SIZE}} red={item.red} green={item.green} blue={item.blue} />
-    </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("DetailsScreen", { ...item })}
+      >
+        <BlockRGB
+          style={{ height: BLOCK_SIZE, width: BLOCK_SIZE }}
+          red={item.red}
+          green={item.green}
+          blue={item.blue}
+        />
+      </TouchableOpacity>
     );
   }
 
@@ -48,15 +57,14 @@ function HomeScreen({ navigation }) {
       },
     ]);
   }
- 
-  function clearColor() {
-      setColorArray([]);
-  }
 
+  function clearColor() {
+    setColorArray([]);
+  }
 
   return (
     <View style={styles.container}>
-       {/* <TouchableOpacity
+      {/* <TouchableOpacity
        style={{ height: 40, justifyContent: "center" }}
        onPress={addColor}
      >
@@ -70,18 +78,22 @@ function HomeScreen({ navigation }) {
        <Text style={{ color: "red" }}>Reset Colour</Text>
      </TouchableOpacity> */}
 
-     <FlatList  style={styles.list} 
-                data={colorArray} 
-                renderItem={renderItem} 
-                numColumns={NUM_COLUMNS}  />
+      <FlatList
+        style={styles.list}
+        data={colorArray}
+        renderItem={renderItem}
+        numColumns={NUM_COLUMNS}
+      />
     </View>
   );
- }
- 
- function DetailsScreen({route}) {
+}
 
+function DetailsScreen({ route }) {
   const { red, green, blue } = route.params;
-  
+
+  const textRed = red > 125 ? 255 - red - 20 : 255 + red + 20;
+  const textGreen = green > 125 ? 255 - green - 20 : 255 + green + 20;
+  const textBlue = blue > 125 ? 255 - blue - 20 : 255 + blue + 20;
 
   return (
     <View
@@ -93,45 +105,60 @@ function HomeScreen({ navigation }) {
         },
       ]}
     >
-      <Text style={styles.detailsText}>Red: {red}</Text>
-      <Text style={styles.detailsText}>Green: {green}</Text>
-      <Text style={styles.detailsText}>Blue: {blue}</Text>
+      <Text
+        style={[
+          { color: `rgb(${textRed}, ${textGreen}, ${textBlue})` },
+          styles.detailsText,
+        ]}
+      >
+        Red: {red}
+      </Text>
+      <Text
+        style={[
+          { color: `rgb(${textRed}, ${textGreen}, ${textBlue})` },
+          styles.detailsText,
+        ]}
+      >
+        Green: {green}
+      </Text>
+      <Text
+        style={[
+          { color: `rgb(${textRed}, ${textGreen}, ${textBlue})` },
+          styles.detailsText,
+        ]}
+      >
+        Blue: {blue}
+      </Text>
     </View>
   );
 }
 
- const Stack = createStackNavigator();
-
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-    
-      <Stack.Screen name="Colorful Kueh Lapis" component={HomeScreen} />
-      <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-
+      <Stack.Navigator>
+        <Stack.Screen name="Colorful Kueh Lapis" component={HomeScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
 
   list: {
     width: "100%",
   },
- 
-  detailsText:{
+
+  detailsText: {
     fontSize: 36,
     marginBottom: 12,
   },
-
-
 });
